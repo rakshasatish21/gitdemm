ffcbfedd13a6a4d85697134b7c3e79b480dcf0ed
rakshasatish21 <46309018+rakshasatish21@users.noreply.github.com>
2019-01-02 14:03:24 +0530

    added print statement

--- first/src/first/fibonacci.java
+++ first/src/first/fibonacci.java
@@ -5,6 +5,7 @@ public class fibonacci
 	public static void main(String[] args)
 	{
 		int fb0=0,fb1=1,fb2;
+		System.out.println("fibonaccci series: ")
 		System.out.print("0 1 ");
 		
 		for(int i=1;i<=10;i++)
@@ -13,7 +14,6 @@ public class fibonacci
 			System.out.print(fb2+" ");
 			fb0=fb1;
 			fb1=fb2;
-			
 		}
 		
 	}
